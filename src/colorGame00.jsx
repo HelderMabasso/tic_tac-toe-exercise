9294c62c98d261ce33989df4b4d59a4b5382494c
helder mabasso <hfmabasso@gmail.com>
2024-05-26 18:53:56 +0200

    adding a onClick function handler named handleClick

--- src/colorGame00.jsx
+++ src/colorGame00.jsx
@@ -3,13 +3,13 @@ import React,{useState} from "react";
 const Board = () => {
   const [player, setPlayer] = useState(0);
   let status = `Player ${player}`;
+  const handleClick = e => {
+    {player === 0 ? setPlayer(1) : setPlayer(0)}
+  }
   return (
     <div
       className="game-board"
-      onClick={(e) => {
-        setPlayer(player + 1);
-        status = `Player ${player}`;
-      }}
+      onClick={handleClick}
     >
       <div id="info">
         <h1>{status}</h1>
