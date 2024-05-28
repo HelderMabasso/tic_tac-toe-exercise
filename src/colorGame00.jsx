ba67dc32931e1d4519ee01e272f2480a85184d70
helder mabasso <hfmabasso@gmail.com>
2024-05-28 20:17:23 +0200

    Creating and rendering square buttons with an id prop

--- src/colorGame00.jsx
+++ src/colorGame00.jsx
@@ -1,19 +1,36 @@
 
 import React,{useState} from "react";
+
+const Square = ({id})=>{
+  return (
+    <button>{id}</button>
+  )
+}
 const Board = () => {
   const [player, setPlayer] = useState(0);
   let status = `Player ${player}`;
   const handleClick = e => {
     {player === 0 ? setPlayer(1) : setPlayer(0)}
+    
   }
+
+  const renderSquare = (i) => {
+    return <Square id={i}></Square>
+  }
+
   return (
-    <div
-      className="game-board"
-      onClick={handleClick}
-    >
+    <div className="game-board">
+
+      <div className="grid-row">
+        {renderSquare(0)}
+        {renderSquare(1)}
+        {renderSquare(2)}
+      </div>
+       
       <div id="info">
         <h1>{status}</h1>
       </div>
+
     </div>
   );
 };
